f604657e6ee5cb25ddfa707c5052bffedd07fbe8
jeremydenton <jeremy.denton9@gmail.com>
2019-05-16 10:40:05 -0400

    Creates event listeners most html and some scss
    event listers using .one so they can only be clicked once. html with
    auth forms and game board. css to put board in grid. created
    folders and files for javascript stuff.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,21 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events')
+const gameEvents = require('./game/events')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-pw').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+  $('#0').one('click', gameEvents.zero)
+  $('#1').one('click', gameEvents.one)
+  $('#2').one('click', gameEvents.two)
+  $('#3').one('click', gameEvents.three)
+  $('#4').one('click', gameEvents.four)
+  $('#5').one('click', gameEvents.five)
+  $('#6').one('click', gameEvents.six)
+  $('#7').one('click', gameEvents.seven)
+  $('#8').one('click', gameEvents.eight)
 })
---
+++ assets/scripts/game/events.js
@@ -0,0 +1,48 @@
+
+const zero = event => {
+  event.preventDefault()
+  console.log('click0')
+}
+const one = event => {
+  event.preventDefault()
+  console.log('click1')
+}
+const two = event => {
+  event.preventDefault()
+  console.log('click2')
+}
+const three = event => {
+  event.preventDefault()
+  console.log('click3')
+}
+const four = event => {
+  event.preventDefault()
+  console.log('click4')
+}
+const five = event => {
+  event.preventDefault()
+  console.log('click5')
+}
+const six = event => {
+  event.preventDefault()
+  console.log('click6')
+}
+const seven = event => {
+  event.preventDefault()
+  console.log('click7')
+}
+const eight = event => {
+  event.preventDefault()
+  console.log('click8')
+}
+module.exports = {
+  zero,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight
+}
